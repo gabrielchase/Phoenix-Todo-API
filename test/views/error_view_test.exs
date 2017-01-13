9fe8e3c0d496743fd7785372a1f69fc6a13a04f6
gchasep <gchase.patron@gmail.com>
2017-01-13 21:55:01 +0800

    initial commit

---
+++ test/views/error_view_test.exs
@@ -0,0 +1,36 @@
+defmodule TodoApi.ErrorViewTest do
+  use TodoApi.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(TodoApi.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(TodoApi.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(TodoApi.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+
+  test "render 404.json" do
+    assert render(TodoApi.ErrorView, "404.json", []) == 
+      %{errors: %{detail: "Page not found"}}
+  end
+
+  test "render 500.json" do
+    assert render(TodoApi.ErrorView, "500.json", []) == 
+      %{errors: %{detail: "Server internal error"}}
+  end
+
+  test "render any other" do
+    assert render(TodoApi.ErrorView, "505.json", []) == 
+      %{errors: %{detail: "Server internal error"}}
+  end
+end
